f3a4e7642bd7f663f689fc4e9c4139c3afc61fb3
Dan Yamins <dyamins@gmail.com>
2012-01-21 13:40:10 -0500

    in midst of merge

--- thoreano/classifier.py
+++ thoreano/classifier.py
@@ -1,16 +1,20 @@
 import numpy as np
 import scipy as sp
 
+import asgd  # use master branch from https://github.com/jaberg/asgd
+from early_stopping import fit_w_early_stopping, EarlyStopping
+from asgd import utils
+
+from scikits.learn import svm as sklearn_svm
+from scikits.learn import linear_model as sklearn_linear_model
+from scikits.learn.linear_model.logistic import LogisticRegression
+
 
 #############
 #####sgd#####
 #############
-import asgd  # use master branch from https://github.com/jaberg/asgd
-from early_stopping import fit_w_early_stopping, EarlyStopping
 
-
-def train_asgd_classifier_normalize(trainXy, testXy, verbose=False, batchsize=10,
-        step_sizes=(3e-4, 1e-4, 3e-5, 1e-5, 3e-6, 1e-6), trace_normalize=False):
+def train_asgd_classifier_normalize(trainXy, testXy, verbose=False, batchsize=10, trace_normalize=False):
 
     train_features, train_labels = trainXy
     test_features, test_labels = testXy
@@ -30,8 +34,7 @@ def train_asgd_classifier_normalize(trainXy, testXy, verbose=False, batchsize=10
 def train_asgd_classifier(train_Xy, test_Xy,
         verbose=False,
         batchsize=10,
-        validation_interval=100,
-        step_sizes=(3e-4, 1e-4, 3e-5, 1e-5, 3e-6, 1e-6)):
+        validation_interval=100):
     """
     batchsize = 10                 # unit: examples
     validation_interval = 100      # unit: batches
@@ -56,11 +59,9 @@ def train_asgd_classifier(train_Xy, test_Xy,
         # -- change labels to -1, +1
         train_y = train_y * 2 - 1
         test_y = test_y * 2 - 1
-        def model_fn(step_size0):
+        def model_fn():
             return asgd.naive_asgd.NaiveBinaryASGD(
-                    n_features=n_features,
-                    l2_regularization=1e-3,
-                    sgd_step_size0=step_size0)
+                    n_features=n_features)
 
         if use_theano:
             print "train_classifier not using theano for binary classif."
@@ -70,14 +71,12 @@ def train_asgd_classifier(train_Xy, test_Xy,
         def model_fn(step_size0):
             return asgd.naive_asgd.NaiveMulticlassASGD(
                     n_features=n_features,
-                    n_classes=len(labelset),
-                    l2_regularization=1e-3,
-                    sgd_step_size0=step_size0)
+                    n_classes=len(labelset))
 
     # -- repeat training for several learning rates
     #    take model that was best on held-out data
-    results = [fit_w_early_stopping(
-                model_fn(step_size0),
+    model, es, test_prediction, train_prediction = fit_w_early_stopping(
+                model_fn(),
                 es=EarlyStopping(warmup=50, max_iters=1000), # unit: validation intervals
                 train_X=train_X,
                 train_y=train_y,
@@ -87,10 +86,7 @@ def train_asgd_classifier(train_Xy, test_Xy,
                 validation_interval=validation_interval,
                 verbose=verbose
                 )
-            for step_size0 in step_sizes]
 
-    results.sort(cmp=lambda a, b: cmp(a[1].best_y, b[1].best_y))
-    model, es, test_prediction, train_prediction = results[0]
     result = get_result(train_y, test_y, train_prediction, test_prediction, labels)
     return model, es, result
 
@@ -98,10 +94,6 @@ def train_asgd_classifier(train_Xy, test_Xy,
 #############
 ###scikits###
 #############
-from scikits.learn import svm as sklearn_svm
-from scikits.learn import linear_model as sklearn_linear_model
-from scikits.learn.linear_model.logistic import LogisticRegression
-
 
 def train_scikits(train_Xy,
                   test_Xy,
@@ -296,10 +288,10 @@ def get_regression_result(train_actual, test_actual, train_predicted, test_predi
     test_results.update(train_results)
     return test_results
 
+
 def regression_stats(actual, predicted, prefix='test'):
-    a_mean = actual.mean()
-    test_rsquared = 1 - np.linalg.norm(actual - predicted)**2 / np.linalg.norm(actual-a_mean)**2
-    return {prefix+'_rsquared' : test_rsquared}
+    return {prefix+'_rsquared' : utils.rsquared(actual, predicted)}
+
 
 def get_result(train_labels, test_labels, train_prediction, test_prediction, labels):
     result = {'train_errors': (train_labels != train_prediction).tolist(),
@@ -332,143 +324,7 @@ def multiclass_stats(test_actual, test_predicted, train_actual, train_predicted,
 
 
 def multiclass_test_stats(test_actual, test_predicted, labels, prefix='test'):
-    test_accuracy = float(100*(test_predicted == test_actual).sum() / float(len(test_predicted)))
-    test_aps = []
-    test_aucs = []
-    if len(labels) == 2:
-        labels = labels[1:]
-    for label in labels:
-        test_prec,test_rec = precision_and_recall(test_actual,test_predicted,label)
-        test_ap = ap_from_prec_and_rec(test_prec,test_rec)
-        test_aps.append(test_ap)
-        test_auc = auc_from_prec_and_rec(test_prec,test_rec)
-        test_aucs.append(test_auc)
-    test_ap = np.array(test_aps).mean()
-    test_auc = np.array(test_aucs).mean()
-    return {prefix+'_accuracy' : test_accuracy,
-            prefix+'_ap' : test_ap,
-            prefix+'_auc' : test_auc}
-
-
-def precision_and_recall(actual,predicted,cls):
-    c = (actual == cls)
-    si = sp.argsort(-c)
-    tp = sp.cumsum(sp.single(predicted[si] == cls))
-    fp = sp.cumsum(sp.single(predicted[si] != cls))
-    rec = tp /sp.sum(predicted == cls)
-    prec = tp / (fp + tp)
-    return prec,rec
-
-
-def ap_from_prec_and_rec(prec,rec):
-    ap = 0
-    rng = sp.arange(0, 1.1, .1)
-    for th in rng:
-        parray = prec[rec>=th]
-        if len(parray) == 0:
-            p = 0
-        else:
-            p = parray.max()
-        ap += p / rng.size
-    return ap
-
-
-def auc_from_prec_and_rec(prec,rec):
-    #area under curve
-    h = sp.diff(rec)
-    auc = sp.sum(h * (prec[1:] + prec[:-1])) / 2.0
-    return auc
-
-
-
-
-#########
-##utils##
-#########
-
-def normalize(feats, trace_normalize=False, data=None):
-    """Performs normalizations before training on a list of feature array/label
-    pairs. first feature array in list is taken by default to be training set
-    and norms are computed relative to that one.
-    """
-
-    if data is None:
-        train_f = feats[0]
-        m = train_f.mean(axis=0)
-        s = np.maximum(train_f.std(axis=0), 1e-8)
-    else:
-        m = data['train_mean']
-        s = data['train_std']
-    feats = [(f - m) / s for f in feats]
-    if trace_normalize:
-        if data is None:
-            train_f = feats[0]
-            tr = np.maximum(np.sqrt((train_f**2).sum(axis=1)).mean(), 1e-8)
-        else:
-            tr = data['trace']
-    else:
-        tr = None
-    if trace_normalize:
-        feats = [f / tr for f in feats]
-    feats = tuple(feats)
-    return feats + (m, s, tr)
-
-
-def mean_and_std(X, min_std):
-    # -- this loop is more memory efficient than numpy
-    #    but not as numerically accurate as possible
-    m = np.zeros(X.shape[1], dtype='float64')
-    msq = np.zeros(X.shape[1], dtype='float64')
-    for i in xrange(X.shape[0]):
-        alpha = 1.0 / (i + 1)
-        v = X[i]
-        m = (alpha * v) + (1 - alpha) * m
-        msq = (alpha * v * v) + (1 - alpha) * msq
-
-    train_mean = np.asarray(m, dtype=X.dtype)
-    train_std = np.sqrt(np.maximum(
-            msq - m * m,
-            min_std ** 2)).astype(X.dtype)
-    return train_mean, train_std
-
-
-def split_center_normalize(X, y,
-        validset_fraction=.2,
-        validset_max_examples=5000,
-        inplace=False,
-        min_std=1e-4,
-        batchsize=1):
-    n_valid = int(min(
-        validset_max_examples,
-        validset_fraction * X.shape[0]))
-
-    # -- increase n_valid to a multiple of batchsize
-    while n_valid % batchsize:
-        n_valid += 1
-
-    n_train = X.shape[0] - n_valid
-
-    # -- decrease n_train to a multiple of batchsize
-    while n_train % batchsize:
-        n_train -= 1
-
-    if not inplace:
-        X = X.copy()
-
-    train_features = X[:n_train]
-    valid_features = X[n_train:n_train + n_valid]
-    train_labels = y[:n_train]
-    valid_labels = y[n_train:n_train + n_valid]
-
-    train_mean, train_std = mean_and_std(X, min_std=min_std)
-
-    # train features and valid features are aliased to X
-    X -= train_mean
-    X /= train_std
-
-    return ((train_features, train_labels),
-            (valid_features, valid_labels),
-            train_mean,
-            train_std)
-
-
+    accuracy, ap, auc = utils.multiclass_stats(test_actual, test_predicted, labels)
+    return {prefix+'_accuracy' : accuracy,
+            prefix+'_ap' : ap,
+            prefix+'_auc' : auc}
